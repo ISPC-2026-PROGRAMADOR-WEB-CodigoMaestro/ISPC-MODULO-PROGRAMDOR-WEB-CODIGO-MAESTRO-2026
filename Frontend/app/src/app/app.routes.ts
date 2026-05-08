c86a6928d8a302e8584a737e1fc50f6725c922e3
guadalupeturri <magaliturri@gmail.com>
2026-05-08 18:55:44 -0300

    Creamos carpeta shared con sus componentes y rutas

--- Frontend/app/src/app/app.routes.ts
+++ Frontend/app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './features/home/home';
+import { Dashboard } from './features/dashboard/dashboard';
+import { QuienesSomos } from './features/quienes-somos/quienes-somos';
+
+export const routes: Routes = [
+
+  { path: '', component: Home },
+
+  { path: 'dashboard', component: Dashboard },
+
+  { path: 'quienes-somos', component: QuienesSomos }
+
+];
