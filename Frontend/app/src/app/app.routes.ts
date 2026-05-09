a29c797adfaa88c553bbc6f79a9450f56e0c1815
guadalupeturri <magaliturri@gmail.com>
2026-05-09 13:40:30 -0300

    Revert "Merge pull request #18 from ISPC-2026-PROGRAMADOR-WEB-CodigoMaestro/feature/GuadalupeTurri"
    This reverts commit 5ea9ccefb48f059965f9d9d00fc9afa45ba13d23, reversing
    changes made to c36c42a4489219bf3dbaa920387db41054e5bccd.

--- Frontend/app/src/app/app.routes.ts
+++ Frontend/app/src/app/app.routes.ts
@@ -1,15 +1,3 @@
 import { Routes } from '@angular/router';
 
-import { Home } from './features/home/home';
-import { Dashboard } from './features/dashboard/dashboard';
-import { QuienesSomos } from './features/quienes-somos/quienes-somos';
-
-export const routes: Routes = [
-
-  { path: '', component: Home },
-
-  { path: 'dashboard', component: Dashboard },
-
-  { path: 'quienes-somos', component: QuienesSomos }
-
-];
+export const routes: Routes = [];
--- Frontend/app/src/app/app.ts
+++ Frontend/app/src/app/app.ts
@@ -1,28 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Header } from './shared/header/header';
-import { Nav } from './shared/nav/nav';
-import { Footer } from './shared/footer/footer';
-
 @Component({
   selector: 'app-root',
-
-  standalone: true,
-
-  imports: [
-    RouterOutlet,
-    Header,
-    Nav,
-    Footer
-  ],
-
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-
   protected readonly title = signal('app');
-
 }
-
