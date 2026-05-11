dc38d5fc5de273b5ecae2e6d6fba01d126312048
Agustin Exequiel Gimenez <exequielgimenez026@gmail.com>
2026-05-11 19:45:38 -0300

    Creacion de los de CRUD en views.py

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,41 +1,29 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets
+
+from .models import Rol, Oficio, Ubicacion, Usuario
+from .serializers import (
+    RolSerializer,
+    OficioSerializer,
+    UbicacionSerializer,
+    UsuarioSerializer
+)
 
 
-@api_view(['POST'])
-def login_view(request):
-
-    email = request.data.get('email')
-
-    password = request.data.get('password')
+class RolViewSet(viewsets.ModelViewSet):
+    queryset = Rol.objects.all()
+    serializer_class = RolSerializer
 
 
-    if email == "profesional@example.com" and password == "password123":
-
-        return Response({
-            'mensaje': 'Login profesional',
-            'redirect': '/perfil'
-        }, status=status.HTTP_200_OK)
+class OficioViewSet(viewsets.ModelViewSet):
+    queryset = Oficio.objects.all()
+    serializer_class = OficioSerializer
 
 
-    elif email == "usuario@example.com" and password == "password123":
-
-        return Response({
-            'mensaje': 'Login usuario',
-            'redirect': '/dashboard'
-        }, status=status.HTTP_200_OK)
+class UbicacionViewSet(viewsets.ModelViewSet):
+    queryset = Ubicacion.objects.all()
+    serializer_class = UbicacionSerializer
 
 
-    return Response({
-        'error': 'Credenciales incorrectas'
-    }, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-@api_view(['POST'])
-def registro_view(request):
-
-    return Response({
-        'mensaje': 'Usuario registrado correctamente'
-    }, status=status.HTTP_201_CREATED)
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
