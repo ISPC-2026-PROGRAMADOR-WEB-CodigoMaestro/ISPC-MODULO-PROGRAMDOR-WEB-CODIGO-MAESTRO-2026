f27b2d0a4d26d09881683dd3acfbf767955afa45
guadalupeturri <magaliturri@gmail.com>
2026-06-01 17:24:52 -0300

    redirección al home después del login

--- Frontend/app/src/app/pages/login/login.ts
+++ Frontend/app/src/app/pages/login/login.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,24 +14,21 @@ export class Login {
 
   loginForm;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
 
     this.loginForm = this.fb.group({
 
       email: [
         '',
-        [
-          Validators.required,
-          Validators.email
-        ]
+        [Validators.required, Validators.email]
       ],
 
       password: [
         '',
-        [
-          Validators.required,
-          Validators.minLength(8)
-        ]
+        [Validators.required, Validators.minLength(8)]
       ]
 
     });
@@ -42,15 +38,15 @@ export class Login {
   ingresar() {
 
     if (this.loginForm.invalid) {
-
       this.loginForm.markAllAsTouched();
-
       return;
-
     }
 
     console.log(this.loginForm.value);
 
+   
+    this.router.navigate(['/']);
   }
 
 }
+
