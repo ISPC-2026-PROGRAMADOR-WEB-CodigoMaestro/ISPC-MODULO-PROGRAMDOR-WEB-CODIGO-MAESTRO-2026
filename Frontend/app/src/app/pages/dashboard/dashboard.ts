2abcbe9c5a5c4650da08f671c1c10e823e41c013
guadalupeturri <magaliturri@gmail.com>
2026-06-01 17:09:19 -0300

    registro de usuario conectado a backend con redirección a login

--- Frontend/app/src/app/pages/dashboard/dashboard.ts
+++ Frontend/app/src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,36 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+
+  usuarios: any[] = [];
+
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+
+    this.usuarioService.getUsuarios().subscribe({
+
+      next: (data) => {
+        this.usuarios = data;
+        console.log(data);
+      },
+
+      error: (error) => {
+        console.error('Error al obtener usuarios:', error);
+      }
+
+    });
+
+  }
+
+}
