5667b0710ae8678ed6a4206533c02a5d7d3d1f8d
Agustin Exequiel Gimenez <exequielgimenez026@gmail.com>
2026-06-01 23:14:19 -0300

    Implementar autenticación de usuarios en frontend

--- Frontend/app/src/app/pages/login/login.ts
+++ Frontend/app/src/app/pages/login/login.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -15,26 +17,12 @@ export class Login {
 
   loginForm;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router, private usuarioService: UsuarioService) {
 
     this.loginForm = this.fb.group({
 
-      email: [
-        '',
-        [
-          Validators.required,
-          Validators.email
-        ]
-      ],
-
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(8)
-        ]
-      ]
-
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
     });
 
   }
@@ -42,14 +30,27 @@ export class Login {
   ingresar() {
 
     if (this.loginForm.invalid) {
-
       this.loginForm.markAllAsTouched();
-
       return;
-
     }
 
-    console.log(this.loginForm.value);
+  const datosLogin = {
+      
+    email: this.loginForm.value.email,
+    contrasena: this.loginForm.value.password
+  };
+
+  this.usuarioService.loginUsuario(datosLogin).subscribe( data => {
+
+    if (data.id > 0)
+    {alert('Login exitoso');
+    this.router.navigate(['/home']);
+    }
+
+    else {this.loginForm.markAllAsDirty();
+    alert('Login fallido, revise sus credenciales');
+    }
+  })
 
   }
 
